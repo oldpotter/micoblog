bcf0f6008692f4e47018ab71d74359535bbe4756
oldpotter <oldpottertom@icloud.com>
2019-01-17 14:30:53 +0800

    修改config

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ load_dotenv(env_file_path)
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or '1234qwer'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(os.getcwd(), 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	POSTS_PER_PAGE = 3 # 每页显示的文章数量
